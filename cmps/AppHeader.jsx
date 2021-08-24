b44d08f3d9843158f60d88251de0effd1313ab98
bcd82 <barak.sidi@gmail.com>
2021-08-24 23:53:22 +0300

    created a mobile header \w/

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,21 +1,33 @@
 const { NavLink, withRouter } = ReactRouterDOM;
 
- const _AppHeader = (props) => {
-  return (
-    <header className="header">
-      <div className="main-header main-layout">
-      <h1 className="logo" onClick={()=>{
-        props.history.push('/')
-      }}>Miss Book</h1>
-      <nav>
-        <NavLink to='/' exact>Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/book" exact>Books</NavLink>
-        <NavLink to="/book/add" exact>Add Book</NavLink>
-      </nav>
-      </div>
-    </header>
-  );
-};
+ class _AppHeader  extends React.Component {
+
+  state = {
+    isMobileMenuOpen : false
+  }
+  
+  toggleMenu = (ev) =>{
+    this.setState({isMobileMenuOpen:!this.state.isMobileMenuOpen})
+  }
+  render() {
+    const {isMobileMenuOpen} = this.state
+    return (
+      <header className="header">
+        <div className="main-header main-layout">
+        <h1 className="logo" onClick={()=>{
+          props.history.push('/')
+        }}>Miss Book</h1>
+        <nav className={isMobileMenuOpen ? 'mobile': ''}>
+          <NavLink to='/' exact onClick={isMobileMenuOpen && this.toggleMenu}>Home</NavLink>
+          <NavLink to="/about" onClick={isMobileMenuOpen && this.toggleMenu}>About</NavLink>
+          <NavLink to="/book" exact onClick={isMobileMenuOpen && this.toggleMenu}>Books</NavLink>
+          <NavLink to="/book/add" exact onClick={isMobileMenuOpen && this.toggleMenu}>Add Book</NavLink>
+         </nav>
+          <button className="menu-btn-mobile" onClick={this.toggleMenu}>☰</button>
+        </div>
+      </header>
+    );
+  };
+  }
 
 export const AppHeader = withRouter(_AppHeader);
